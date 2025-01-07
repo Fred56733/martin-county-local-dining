2c7b24a63cae9c521897dac6be5843b688f0aefb
Fred56733 <fdebiase2020@fau.edu>
2025-01-06 23:57:26 -0500

    logo location change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from './assets/martincountydining.png';
 import CardList from "./components/CardList";
 
 const App = () => {
     return (
         <div className="App">
             <header className="header">
+                <img src={logo} alt="Martin County Dining Logo" className="header-logo" />
                 <div className="header-banner">
                     <h1>Martin County Dining Hot Spots</h1>
                     <p>Martin County local tested and approved</p>
